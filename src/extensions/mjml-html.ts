a75f97fd6a19b8ecca740103ba62718a2cb7f366
Harrison Chiu <harrison.chiu@faire.com>
2022-10-20 10:56:06 -0400

    make tag prop optional, rename nonjsx files to ts

--- src/extensions/mjml-html.ts
+++ src/extensions/mjml-html.ts
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 
 interface IMjmlHtml {
-  tag: string;
+  tag?: string;
   html: string;
 }
 
 export class MjmlHtml extends Component<IMjmlHtml> {
   render() {
-    const { tag, html } = this.props;
-    return React.createElement(tag || "mj-raw", {
+    const { tag = "mj-raw", html } = this.props;
+    return React.createElement(tag, {
       dangerouslySetInnerHTML: {
         __html: html,
       },
