3a7b97bed540110f7acfe45b88f9b60f421177bc
Evan McLaughlin <evan.mclaughlin@faire.com>
2022-10-25 11:15:35 -0400

    update naming of endingTag

--- scripts/generate-mjml-react.ts
+++ scripts/generate-mjml-react.ts
@@ -132,7 +132,11 @@ function getPropTypeFromMjmlAttributeType(
 }
 
 function buildTypesForComponent(mjmlComponent: IMjmlComponent): string {
-  const { componentName, allowedAttributes, endingTag } = mjmlComponent;
+  const {
+    componentName,
+    allowedAttributes,
+    endingTag: isEndingTag,
+  } = mjmlComponent;
   const typesFromMjmlAttributes: Record<string, string> = {};
   if (allowedAttributes) {
     Object.entries(allowedAttributes).forEach(
@@ -170,7 +174,7 @@ function buildTypesForComponent(mjmlComponent: IMjmlComponent): string {
   if (HAS_CHILDREN.has(componentName)) {
     typesFromMjmlAttributes["children"] = "React.ReactNode";
   }
-  if (endingTag) {
+  if (isEndingTag) {
     typesFromMjmlAttributes["dangerouslySetInnerHTML"] = "{ __html: string }";
   }
 
