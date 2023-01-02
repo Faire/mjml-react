aee93795a0f911ca686e3686910ffd0057bd4473
Ian Edington <ian@faire.com>
2023-01-02 13:18:16 -0500

    fix(components): Add `MjmlSelector` and `MjmlHtmlAttribute`
    fixes: #74

---
+++ src/mjml/MjmlHtmlAttribute.tsx
@@ -0,0 +1,23 @@
+/*
+ * This file is generated. Don't edit it directly.
+ * Modify `scripts/generate-mjml-react.ts` to make changes to these files
+ */
+import React from "react";
+
+import { convertPropsToMjmlAttributes } from "../utils";
+
+export interface IMjmlHtmlAttributeProps {
+  children?: React.ReactNode;
+  name?: string;
+}
+
+export function MjmlHtmlAttribute({
+  children,
+  ...props
+}: IMjmlHtmlAttributeProps): JSX.Element {
+  return React.createElement(
+    "mj-html-attribute",
+    convertPropsToMjmlAttributes(props),
+    children
+  );
+}
