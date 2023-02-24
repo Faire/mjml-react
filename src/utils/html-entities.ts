e660b6e934cf1de636d8ab9c8ddfbe1c74c4e3d6
Evan McLaughlin <70530636+emmclaughlin@users.noreply.github.com>
2023-02-24 09:58:45 -0500

    fix(import): convert require to esm import (#98)

--- src/utils/html-entities.ts
+++ src/utils/html-entities.ts
@@ -1,9 +1,10 @@
-const entities = require("./html-entities.json");
+import entities from "./html-entities.json";
 
 export function namedEntityToHexCode(html: string): string {
   return html.replace(/&([a-z0-9]{2,8});/gi, (match, p1) => {
-    if (entities[p1]) {
-      return `&#${entities[p1]};`;
+    const anyEntities = entities as any;
+    if (anyEntities[p1]) {
+      return `&#${anyEntities[p1]};`;
     }
     return match;
   });
